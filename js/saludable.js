9b4c1c8de0a8d0b201d5f9f48b3d8add931aab98
DaatX <113459239+DaatX@users.noreply.github.com>
2024-05-02 13:32:40 -0400

    Add files via upload

---
+++ js/saludable.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    // activar menu de hambuguesa
+    $('#menu').click(function() {
+
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+
+    });
+    // desativar menu de hambuguesa
+    $(window).on('load scroll', function() {
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if ($(window).scrollTop() > 0) {
+            $('#scroll-top').show();
+        } else {
+            $('#scroll-top').hide();
+        }
+
+    });
+
+    // desplazamiento suave
+
+    $('a[href*="#"]').on('click', function(e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+                scrollTop: $($(this).attr('href')).offset().top,
+
+            },
+            500,
+            'linear'
+        );
+
+    });
+
+});
